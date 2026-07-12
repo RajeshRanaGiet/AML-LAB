74871c9251c673dde6def8d9ff99084a6138e445
RajeshRanaGiet <rajeshjis7978@gmail.com>
2026-07-12 14:59:37 +0530

    Update app.py

--- Experiment 1/app.py
+++ Experiment 1/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import matplotlib.pyplot as plt
 import numpy as np
-import os
 app = Flask(__name__)
 
 @app.route("/")
@@ -29,26 +28,8 @@ def visualize_regression():
     plt.savefig(image1_path)
     plt.close()
 
-    n = len(x)
-    mae = 0
-    mse = 0
-    for i in range(n):
-        error = y[i] - y_pred[i]
-        mae += abs(error)
-        mse += error ** 2
-    mae = mae / n
-    mse = mse / n
-    rmse = mse ** 0.5
-    mean_y = sum(y) / n
-    ss_res = 0
-    ss_tot = 0
-    for i in range(n):
-        ss_res += (y[i] - y_pred[i]) ** 2
-        ss_tot += (y[i] - mean_y) ** 2
-    r2 = 1 - (ss_res / ss_tot)
-
     metrics = ["MSE", "MAE", "RMSE", "R2"]
-    values = [mse, mae, rmse, r2]
+    values = [data["mse"], data["mae"], data["rmse"], data["r2"]]
     plt.figure(figsize=(6,4))
     plt.scatter(metrics, values)
     for i in range(4):
